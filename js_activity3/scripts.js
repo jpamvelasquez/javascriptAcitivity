d9fdd6cd23dc865e431b6c67eec6dd2e1c4082e0
jpamvelasquez <caspajeve@gmail.com>
2024-03-12 00:05:43 +0800

    initial

---
+++ js_activity3/scripts.js
@@ -0,0 +1,23 @@
+const multiply = document.querySelector(".ops-mul");
+
+multiply.addEventListener("click", function () {
+  let rows = parseInt(document.querySelector(".type-row").value);
+  let cols = parseInt(document.querySelector(".type-col").value);
+
+  let spacing = "";
+
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= cols; j++) {
+      spacing +=
+        "<span class='num-space' style='height:4rem; width:4rem'>" +
+        i * j +
+        " " +
+        "</span>";
+    }
+    spacing += "<div class='margin'></div>";
+  }
+
+  document.querySelector(".output-here").innerHTML = spacing;
+  document.querySelector(".type-row").value = "";
+  document.querySelector(".type-col").value = "";
+});
