37934262f6d4c9b3b187d1ea90fa66e540f19b87
jpamvelasquez <caspajeve@gmail.com>
2024-03-14 20:38:33 +0800

    edit button

--- js_activity5/script.js
+++ js_activity5/script.js
@@ -1,5 +1,7 @@
-let logHours = document.getElementById("e.log");
+"use strict";
 
+let logHours = document.getElementById("e.log");
+let updateIndex = -1;
 let info = [];
 
 logHours.addEventListener("click", function () {
@@ -11,6 +13,14 @@ logHours.addEventListener("click", function () {
 
   if (!employeeHours || !employeeName || !employeeId) {
     alert("Invalid Input");
+  } else if (updateIndex !== -1) {
+    info[updateIndex] = [employeeId, employeeName, employeeHours];
+
+    updateIndex = -1;
+
+    document.getElementById("e.id").value = "";
+    document.getElementById("e.name").value = "";
+    document.getElementById("e.hours").value = "";
   } else {
     info.push([employeeId, employeeName, employeeHours]);
     document.getElementById("e.id").value = "";
@@ -30,7 +40,8 @@ function displayInfo() {
             <td> ${info[x][0]} </td> 
             <td> ${info[x][1]} </td> 
             <td> ${info[x][2]} </td> 
-            <td> <button class='btn btn-danger removes' onclick='remove(${x})'> Remove</button> </td>
+            <td> <button class='btn btn-danger removes' onclick='remove(${x})'> Remove</button> <button class='btn  btn-success' onclick='update(${x})'> Edit  </button>  </td>
+          
             </tr>`;
     total += Number(info[x][2]);
   }
@@ -41,4 +52,12 @@ function displayInfo() {
 function remove(index) {
   info.splice(index, 1);
   displayInfo();
+}
+
+function update(index) {
+  updateIndex = index;
+  document.getElementById("e.id").value = info[index][0];
+  document.getElementById("e.name").value = info[index][1];
+  document.getElementById("e.hours").value = info[index][2];
+  document.getElementById("e.id").disabled = true;
 }
