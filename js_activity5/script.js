e8a47c2353a8eba9235f5f37ef2052dad7933501
jpamvelasquez <caspajeve@gmail.com>
2024-03-14 21:33:50 +0800

    chnages

--- js_activity5/script.js
+++ js_activity5/script.js
@@ -14,7 +14,8 @@ logHours.addEventListener("click", function () {
   if (!employeeHours || !employeeName || !employeeId) {
     alert("Invalid Input");
   } else if (updateIndex !== -1) {
-    info[updateIndex] = [employeeId, employeeName, employeeHours];
+    let originalEmployeeId = info[updateIndex][0];
+    info[updateIndex] = [originalEmployeeId, employeeName, employeeHours];
 
     updateIndex = -1;
 
@@ -22,6 +23,10 @@ logHours.addEventListener("click", function () {
     document.getElementById("e.name").value = "";
     document.getElementById("e.hours").value = "";
   } else {
+    if (idExist(employeeId)) {
+      alert("Employee ID already exists");
+      return;
+    }
     info.push([employeeId, employeeName, employeeHours]);
     document.getElementById("e.id").value = "";
     document.getElementById("e.name").value = "";
@@ -29,7 +34,8 @@ logHours.addEventListener("click", function () {
   }
   console.log(info);
   displayInfo();
-  document.getElementById("e.id").disabled = false;
+
+  document.querySelector(".updating").innerText = "Log Hours";
 });
 
 function displayInfo() {
@@ -57,8 +63,14 @@ function remove(index) {
 
 function update(index) {
   updateIndex = index;
-  document.getElementById("e.id").value = info[index][0];
+  let originalId = (document.getElementById("e.id").value = info[index][0]);
+  document.getElementById("e.id").value = info[index[0]];
   document.getElementById("e.name").value = info[index][1];
   document.getElementById("e.hours").value = info[index][2];
-  document.getElementById("e.id").disabled = true;
+
+  document.querySelector(".updating").innerText = "Update Form";
+}
+
+function idExist(id) {
+  return info.some((empId) => empId[0] === id);
 }
