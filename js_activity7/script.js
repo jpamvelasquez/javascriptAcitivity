05b46ce059eca7658009b1762705d18355d5d5da
jpamvelasquez <caspajeve@gmail.com>
2024-03-19 00:37:41 +0800

    dom

---
+++ js_activity7/script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+let add = document.querySelector(".add-grade");
+let calculate = document.querySelector(".calc-grade");
+let remove = document.querySelector(".remove-grade");
+
+add.addEventListener("click", function () {
+  //   alert("oks");
+
+  let inputGrade = document.createElement("input");
+  inputGrade.className = "form-control display-grade";
+  inputGrade.type = "number";
+  inputGrade.placeholder = "Input Grade";
+  let li = document.createElement("li");
+  li.className = "mt-2 p-2";
+  li.append(inputGrade);
+  document.querySelector(".li-parent").append(li);
+});
+
+calculate.addEventListener("click", function () {
+  //alert('oks')
+  let grades = document.querySelectorAll(".display-grade");
+  let total = 0;
+  let gradeCount = 0;
+
+  grades.forEach(function (el) {
+    if (el.value !== "") {
+      total += Number(el.value);
+      gradeCount++;
+      console.log(total);
+      console.log(gradeCount);
+    }
+  });
+
+  let averageGrade = total / gradeCount;
+  document.querySelector("span").innerText = averageGrade.toFixed(2);
+});
+
+remove.addEventListener("click", function () {
+  //alert('oks')
+  let grades = document.querySelectorAll(".display-grade");
+
+  let lastElement = document.querySelector("li:last-child");
+  if (lastElement) {
+    lastElement.remove();
+  }
+
+  grades = document.querySelectorAll(".display-grade");
+
+  if (grades.length === 0) {
+    document.querySelector("span").innerText = "00.00";
+    return;
+  }
+
+  let total = 0;
+  let gradeCount = 0;
+  grades.forEach(function (el) {
+    total += Number(el.value);
+    gradeCount++;
+  });
+
+  let averageGrade = total / gradeCount;
+  document.querySelector("span").innerText = averageGrade.toFixed(2);
+});
